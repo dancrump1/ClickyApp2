0b76305eb575309e39c0ed91b9fb16769373260f
dancrump1 <42187710+dancrump1@users.noreply.github.com>
2019-02-12 20:15:24 -0500

    Updates

---
+++ precache-manifest.0c1b6b46d387cd2be50be8d772c0370b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f10c1d116bf34c7b7d4d",
+    "url": "/ClickyApp2/static/js/runtime~main.f10c1d11.js"
+  },
+  {
+    "revision": "82759ee2fbf3c6a6a5cc",
+    "url": "/ClickyApp2/static/js/main.82759ee2.chunk.js"
+  },
+  {
+    "revision": "4cbed206c501c3870381",
+    "url": "/ClickyApp2/static/js/2.4cbed206.chunk.js"
+  },
+  {
+    "revision": "82759ee2fbf3c6a6a5cc",
+    "url": "/ClickyApp2/static/css/main.6b81a509.chunk.css"
+  },
+  {
+    "revision": "ebc141b9865573bca023626c7218ceae",
+    "url": "/ClickyApp2/index.html"
+  }
+];
